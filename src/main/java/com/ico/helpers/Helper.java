823c5931e889d6c025016b90f9e35c14ba87650b
Manpreet Singh <98836538+ManpreetSingh365@users.noreply.github.com>
2024-09-29 01:04:47 +0530

    Update Helper.java

--- src/main/java/com/ico/helpers/Helper.java
+++ src/main/java/com/ico/helpers/Helper.java
@@ -50,7 +50,8 @@ public class Helper {
 
     public String getLinkForEmailVerificatiton(String emailToken) {
 
-        return this.baseUrl + "/auth/verify-email?token=" + emailToken;
+        // return this.baseUrl + "/auth/verify-email?token=" + emailToken;
+        return this.baseUrl + "auth/verify-email?token=" + emailToken;
 
     }
 }
